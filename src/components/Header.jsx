2f2e47b1e06b6d56b8540017ee19663d82f17d68
Nadeem Ahmad <89350768+enn-dee@users.noreply.github.com>
2022-12-25 12:51:08 +0000

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+// import React from "react";
+function Header() {
+  return (
+    <header>
+      <div className="container">
+        <h2>Feedback UI</h2>
+      </div>
+    </header>
+  )
+}
+export default Header
