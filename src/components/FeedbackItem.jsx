d6127e3de5b17d3f7e6c61bbe441fd1a93f4c53b
Nadeem Ahmad <89350768+enn-dee@users.noreply.github.com>
2022-12-27 14:41:36 +0000

    add useState

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,16 @@
-import {useState} from 'react'
-function FeedbackItem(){
+import { useState } from 'react'
+function FeedbackItem() {
+  const [rating, setRating] = useState(7);
 
-  return(
+  const [text, setText] = useState("This is an exmaple of a feedback item")
+  
+  return (
     <div className='card'>
-      <div className='num-display'>10</div>
-      <div className= 'text-display'>This is text to display</div>
+      <div className='num-display'>{rating}</div>
+      <div className='text-display'>{text}</div>
+    
     </div>
+
   )
 }
 
